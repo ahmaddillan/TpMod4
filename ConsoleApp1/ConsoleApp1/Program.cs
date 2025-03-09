a2d019b191afe6323e2599f23231b56747cf79eb
ahmaddillan <ahmad.dillan@gmail.com>
2025-03-09 21:14:54 +0700

    menambahkan implementasi dengan table-driven

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Kodepos {
+    public static string getKodePos(string daerah)
+    {
+        string[,] dataKodePos = {
+            { "Batununggal", "40266" },
+            { "Kujangsari", "40287" },
+            { "Mengger", "40267" },
+            { "Wates", "40256" },
+            { "Cijaura", "40287" },
+            { "Jatisari", "40286" },
+            { "Margasari", "40286" },
+            { "Sekejati", "40286" },
+            { "Kebonwaru", "40272" },
+            { "Maleer", "40274" },
+            { "Samoja", "40273" }
+        };
+
+        for (int i = 0; i < dataKodePos.GetLength(0); i++) {
+            if (dataKodePos[i, 0].Equals(daerah, StringComparison.OrdinalIgnoreCase))
+            {
+                return dataKodePos[i, 1];
+            }
+        }
+        return "kode pos tidak ditemukan";
+    }
+}
+
+public class Program {
+    public static void Main() {
+        Kodepos Kode = new Kodepos();
+
+        Console.WriteLine("masukkan nama kelurahan : ");
+        string kelurahan = Console.ReadLine() ?? "";
+
+        string kode = Kodepos.getKodePos(kelurahan);
+        Console.WriteLine($"kode pos {kelurahan} : {kode}");
+    }
+}
--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46a72467915a195d6fbad506e2a2ce41f915a53a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
